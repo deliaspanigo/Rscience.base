21cae8808d4804a9d8e8060d438abb013c05bda7
deliaspanigo <d.eliaspanigo@gmail.com>
2022-12-22 16:33:16 -0300

    img03

--- R/round2.R
+++ R/round2.R
@@ -1,9 +1,9 @@
-round2 <- function(x, n) {
+round2 <- function(x, digits) {
   posneg <- sign(x)
 
-  z <- abs(x)*10^n
+  z <- abs(x)*10^digits
   z <- z + 0.5
   z <- trunc(z)
-  z <- z/10^n
+  z <- z/10^digits
   z*posneg
 }
--- tests/testthat/test-round2.R
+++ tests/testthat/test-round2.R
@@ -1,18 +1,18 @@
 test_that("round2 test - 0.5 con cero digitos devuelve 1.", {
-  expect_equal(round2(0.5, n = 0), 1)
+  expect_equal(round2(0.5, digits = 0), 1)
 })
 
 
 test_that("round2 test - 0.5 con un digito devuelve 1.", {
-  expect_equal(round2(0.5, n = 1), 0.5)
+  expect_equal(round2(0.5, digits = 1), 0.5)
 })
 
 
 test_that("round2 test - 0.4 con cero digitos devuelve 0.", {
-  expect_equal(round2(0.4, n = 0), 0)
+  expect_equal(round2(0.4, digits = 0), 0)
 })
 
 
 test_that("round2 test - 0.4 con un digito devuelve 1.", {
-  expect_equal(round2(0.4, n = 1), 0.4)
+  expect_equal(round2(0.4, digits = 1), 0.4)
 })
