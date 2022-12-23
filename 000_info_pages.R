18bddb48f05b62fe9fa7d8da37533c0c4b82df91
deliaspanigo <d.eliaspanigo@gmail.com>
2022-12-23 16:29:44 -0300

    Paginas Web

--- 000_info_pages.R
+++ 000_info_pages.R
@@ -1,2 +1,23 @@
 # Rmarkdown en GitHub
 # https://mbounthavong.com/blog/2022/7/30/hosting-a-r-markdown-html-file-on-a-github-page
+
+# Testing
+https://www.r-bloggers.com/2019/11/automated-testing-with-testthat-in-practice/
+
+# Cosas de estetica para el readme de github
+https://www.aluracursos.com/blog/como-escribir-un-readme-increible-en-tu-github?gclid=CjwKCAiAnZCdBhBmEiwA8nDQxXWWgwYIcigtKU2LdPqXQij8E84JRx5GSvUhTWrd8teVUINfqh4N_xoCVckQAvD_BwE
+
+
+# Hostear codigo HTML en github
+https://mbounthavong.com/blog/2022/7/30/hosting-a-r-markdown-html-file-on-a-github-page
+
+
+# Pagina de presentaciones original
+https://ppaccioretti.github.io/EstadisticaEspacial/slides.html#/estimaci%C3%B3n-frecuentista-y-bayesiana-de-modelos-lineales-para-datos-con-correlaci%C3%B3n-espacial
+
+
+# Pagina full de Analisis Espacial
+https://ppaccioretti.github.io/EstadisticaEspacial/Ej01.html
+
+# Pagina github de ANalisis Espacial
+https://github.com/PPaccioretti/EstadisticaEspacial
